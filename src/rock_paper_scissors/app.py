c06e449587f181e9c21b2e741275b91ee1bafb0d
xinyu <xinyu@phieso.local>
2023-03-21 22:27:42 -0700

    black formated

--- src/rock_paper_scissors/app.py
+++ src/rock_paper_scissors/app.py
@@ -26,27 +26,29 @@ class InvalidMove(Exception):
     Invalid Move
     """
 
+
 app = Flask(__name__)
 
 
 @app.route("/health")
 def health():
-    '''check health'''
+    """check health"""
     return "OK"
 
-@app.route("/rps", methods = ['POST'])
+
+@app.route("/rps", methods=["POST"])
 def rps():
-    '''rps function'''
+    """rps function"""
     # Create number to choice mapping
     mapping = ["Rock", "Paper", "Scissors"]
 
-    move = request.json.get('move', '')
+    move = request.json.get("move", "")
     try:
         user_choice = mapping.index(move.lower().capitalize())
     except ValueError as exc:
         raise InvalidMove(f"{move} is invalid. Valid moves: {mapping}") from exc
 
-    game_result, pc_choice  = rock_paper_scissors(user_choice)
+    game_result, pc_choice = rock_paper_scissors(user_choice)
     if game_result == 0:
         result = "Tie"
     elif game_result == -1:
@@ -54,6 +56,6 @@ def rps():
     elif game_result == 1:
         result = f"You win, {move} beats {mapping[pc_choice]}"
 
-    return json.dumps({'result': result,
-                       'game_result': game_result,
-                       'pc_choice': pc_choice})
+    return json.dumps(
+        {"result": result, "game_result": game_result, "pc_choice": pc_choice}
+    )
