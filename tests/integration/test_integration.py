31ed01c8d0bff9e12b3cf18087d4bcfd0565c290
Eun Ju Jong <89494794+eunjujong@users.noreply.github.com>
2023-03-30 07:51:35 -0700

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -7,14 +7,14 @@ import requests, os
 
 def test_health():
     """testing health check function"""
-    res = requests.get(os.environ["ENDPOINT"] + "/health")
+    res = requests.get(os.environ.get("ENDPOINT", "http://localhost:7000") + "/health")
     assert res.content == b"OK"
 
 
 def test_psr():
     """testing rps function"""
     res = requests.post(
-        os.environ.get("ENDPOINT", "http://localhost:5000") + "/rps",
+        os.environ.get("ENDPOINT", "http://localhost:7000") + "/rps",
         json={"move": "rock"},
     )
     assert res.status_code == 200
