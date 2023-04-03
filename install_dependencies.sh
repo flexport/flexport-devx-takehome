eefe05c8dcfcb4a7ca1e910727d7413dde6bcf11
Ashwin Meena Meiyappan <ashwinmeenameiyappan@gmail.com>
2023-04-02 17:27:57 -0700

    modified shell script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,14 @@
 # Install Python 3.9.12 using Homebrew package manager
 brew install python@3.9
 
+# Download the get-pip.py script
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+# Install pip with sudo
+sudo python3 get-pip.py
+
 pip install --upgrade pip
+
+# Install all dependencies on remote server
 pip install Flask==2.2.3 pytest==7.2.2 black==23.3.0 pylint==2.17.1 coverage==7.2.2
 
