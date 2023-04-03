66249abfcd6536d18168860cec8fc070e8b58fb0
Ashwin Meena Meiyappan <ashwinmeenameiyappan@gmail.com>
2023-04-02 19:00:07 -0700

    adding pip3.9

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,8 +10,8 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # Install pip with sudo
 sudo python3 get-pip.py
 
-pip install --upgrade pip
+pip3.9 install --upgrade pip
 
 # Install all dependencies on remote server
-python3 pip install flask==2.2.3 pytest==7.2.2 black==23.3.0 pylint==2.17.1 coverage==7.2.2
+pip3.9 install flask==2.2.3 pytest==7.2.2 black==23.3.0 pylint==2.17.1 coverage==7.2.2
 
