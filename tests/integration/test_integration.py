1e05f071146dd805f0be9ae722c89a07982e7793
Yihan Tian <yihantian@Yihans-MBP-2.attlocal.net>
2023-04-11 00:03:21 -0700

    fix integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -12,11 +12,11 @@ def test_game():
     """
     Test the Rock Paper Scissors Game Play
     """
-    payload = {"move": "Rock"}
+    payload = '{"move": "Rock"}'
     headers = {"Content-Type": "application/json"}
-    resp = requests.post("http://127.0.0.1:5000/rps", data=payload, headers=headers)
+    resp = requests.post("http://127.0.0.1:8000/rps", data=payload, headers=headers)
     assert resp.status_code == 200
-    pc_choice, game_result, result = resp.json()
+    pc_choice, game_result, result = resp.json()['pc_choice'], resp.json()['game_result'], resp.json()['result']
     if pc_choice == 0:
         assert game_result == 0
         assert result == "Tie"
@@ -26,3 +26,5 @@ def test_game():
     else:
         assert game_result == 1
         assert result == "You win, Rock beats Scissors"
+
+test_game()
