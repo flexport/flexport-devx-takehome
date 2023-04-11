f2215129f6a45a927a49a45a69bc18e3ad47b974
Yihan Tian <yihantian@Yihans-MBP-2.attlocal.net>
2023-04-10 20:12:21 -0700

    add tests in integration

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,28 @@
+import requests
+
+def test_health():
+    """
+    Health Check for Rock Paper Scissors Application
+    """
+    resp = requests.get("http://127.0.0.1:5000/health")
+    assert resp.status_code == 200
+
+
+def test_game():
+    """
+    Test the Rock Paper Scissors Game Play
+    """
+    payload = {"move": "Rock"}
+    headers = {"Content-Type", "application/json"}
+    resp = requests.post("http://127.0.0.1:5000", data=payload, headers=headers)
+    assert resp.status_code == 200
+    pc_choice, game_result, result = resp.json()
+    if pc_choice == 0:
+        assert game_result == 0
+        assert result == "Tie"
+    elif pc_choice == 1:
+        assert game_result == -1
+        assert result == "I win, Paper beats Rock"
+    else:
+        assert game_result == 1
+        assert result == "You win, Rock beats Scissors"
