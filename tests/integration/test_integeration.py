2ae79e97ba6f760c7fbd9fb1b6bf744fe6a93a55
Ashwin Meena Meiyappan <ashwinmeenameiyappan@gmail.com>
2023-04-11 21:51:24 -0700

    added integration tests

--- tests/integration/test_integeration.py
+++ tests/integration/test_integeration.py
@@ -3,7 +3,6 @@ import requests
 
 URL = "http://127.0.0.1:5000"
 
-
 def test_health():
     """
     Testing the /health endpoint
@@ -12,4 +11,26 @@ def test_health():
     assert response.status_code == 200
     assert response.content == b"OK"
 
-    print("OK")
+def test_rps_with_valid_input():
+    """
+    Testing the /rps with valid input post request
+    """
+    data = {"move": "Rock"}
+    response = requests.post(f"{URL}/rps", json=data)
+    assert response.status_code == 200
+
+    data = {"move": "Paper"}
+    response = requests.post(f"{URL}/rps", json=data)
+    assert response.status_code == 200
+
+    data = {"move": "Scissors"}
+    response = requests.post(f"{URL}/rps", json=data)
+    assert response.status_code == 200
+
+def test_rps_with_invalid_input():
+    """
+    Testing the /rps with invalid input post request
+    """
+    data = {"move": "Papers"}
+    response = requests.post(f"{URL}/rps", json=data)
+    assert response.status_code == 500
