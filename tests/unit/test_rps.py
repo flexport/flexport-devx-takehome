ab46983608674fb5d2aa51ae73b2546b51e42991
kevtom404 <111010526+kevtom404@users.noreply.github.com>
2023-05-17 17:57:11 -0500

    Update test_rps.py

--- tests/unit/test_rps.py
+++ tests/unit/test_rps.py
@@ -2,9 +2,53 @@ import pytest
 from rock_paper_scissors.rps import rock_paper_scissors
 
 
-def test_rps():
+def test_rps(mocker):
     """
     Basic test for Rock Paper Scissors
     """
 
-    assert rock_paper_scissors(1) is not None
+    # When pc chooses rock move
+    mocker.patch("rps.rock_paper_scissors.random.randint", return_value=0)
+
+    game_result, pc_choice = rock_paper_scissors(0)
+    assert game_result == 0
+    assert pc_choice == 0
+
+    game_result, pc_choice = rock_paper_scissors(1)
+    assert game_result == 1
+    assert pc_choice == 0
+
+    game_result, pc_choice = rock_paper_scissors(2)
+    assert game_result == -1
+    assert pc_choice == 0
+
+    # When pc chooses paper move
+    mocker.patch("rps.rock_paper_scissors.random.randint", return_value=1)
+
+    game_result, pc_choice = rock_paper_scissors(0)
+    assert game_result == -1
+    assert pc_choice == 1
+
+    game_result, pc_choice = rock_paper_scissors(1)
+    assert game_result == 0
+    assert pc_choice == 1
+
+    game_result, pc_choice = rock_paper_scissors(2)
+    assert game_result == 1
+    assert pc_choice == 1
+
+    # When pc chooses scissor move
+    mocker.patch("rps.rock_paper_scissors.random.randint", return_value=2)
+
+    game_result, pc_choice = rock_paper_scissors(0)
+    assert game_result == 1
+    assert pc_choice == 2
+
+    game_result, pc_choice = rock_paper_scissors(1)
+    assert game_result == -1
+    assert pc_choice == 2
+
+    game_result, pc_choice = rock_paper_scissors(2)
+    assert game_result == 0
+    assert pc_choice == 2
+
