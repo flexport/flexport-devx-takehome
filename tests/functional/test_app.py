03ee14e23e677b2dcb06ef71a32cffcd56cd5e94
kevtom404 <111010526+kevtom404@users.noreply.github.com>
2023-05-17 19:21:17 -0500

    Update test_app.py
    Added extra functional tests

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -2,14 +2,29 @@ import json
 
 from rock_paper_scissors.app import app
 
-
-def test_rps():
-    """
+"""
     Test Flask Application and API for Rock Paper Scissors
-    """
+"""
 
+
+def test_app_health():
+    with app.test_client() as test_client:
+        response = test_client.get("/healthy")
+        assert response.status_code == 200
+        assert response.data == "OK"
+
+
+def test_rps_valid_move():
     with app.test_client() as test_client:
         response = test_client.post(
             "/rps", data=json.dumps(dict(move="Rock")), content_type="application/json"
         )
         assert response.status_code == 200
+
+
+def test_rps_invalid_move():
+    with app.test_client() as test_client:
+        response = test_client.post(
+            "/rps", data=json.dumps(dict(move="Stone")), content_type="application/json"
+        )
+        assert response.status_code == 500
